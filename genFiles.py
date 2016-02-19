debeae6cd75a441f798d6b3615fa39d2489d96da
Raedslab <raed@riseup.net>
2016-02-19 14:23:04 +0100

    added output

--- genFiles.py
+++ genFiles.py
@@ -3,8 +3,9 @@
 first = []
 second = []
 third = []
+output = []
 
-# Read (format : 0 1 11 111)
+# Read (format : T E1 E2 E3 S)
 sourceFile = open("source/source.csv", "r")
 sourceLines = sourceFile.readlines()
 sourceFile.close()
@@ -12,13 +13,17 @@ sourceFile.close()
 i = 0
 # Process
 for line in sourceLines:
-    line = line.rstrip('\n')
-    values = line.split()
-    first.append(str(values[0]) + " " + str(values[1]) + "\n")
-    second.append(str(values[0]) + " " + str(values[2]) + "\n")
-    third.append(str(values[0]) + " " + str(values[3]) + "\n")
+    if i > 0:
+        line = line.rstrip('\n')
+        values = line.split()
+        first.append(str(values[0]) + " " + str(values[1]) + "\n")
+        second.append(str(values[0]) + " " + str(values[2]) + "\n")
+        third.append(str(values[0]) + " " + str(values[3]) + "\n")
+        output.append(str(values[0]) + " " + str(values[4]) + "\n")
+
     i += 1
 
+i -= 1
 first[i - 1] = first[i - 1].rstrip('\n')
 second[i - 1] = second[i - 1].rstrip('\n')
 third[i - 1] = third[i - 1].rstrip('\n')
@@ -33,7 +38,12 @@ fh = open("gen/second.csv", "w")
 fh.writelines(second)
 fh.close()
 
-# Write 2
+# Write 3
 fh = open("gen/third.csv", "w")
 fh.writelines(third)
+fh.close()
+
+# Write Output
+fh = open("gen/output.csv", "w")
+fh.writelines(output)
 fh.close()
