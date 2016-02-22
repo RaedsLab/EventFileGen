576540961d2e71e296b9031beb9929d69fbdf97c
Raedslab <raed@riseup.net>
2016-02-22 12:37:56 +0100

    better tests using time

--- resTest.py
+++ resTest.py
@@ -6,7 +6,7 @@ sourceLines = sourceFile.readlines()
 sourceFile.close()
 
 destLines = []
-
+realVals = []
 i = 0
 for source in sourceLines:
     outline = ""
@@ -14,6 +14,7 @@ for source in sourceLines:
     time = int(round(float(source.split("=")[2].split('>')[0])))
     outline = str(time) + " " + str(value) + "\n"
     destLines.append(outline)
+    realVals.append({"value": str(value), "time": str(time)})
     i += 1
 
 destLines[i - 1] = destLines[i - 1].rstrip('\n')
@@ -26,15 +27,31 @@ genFile = open("gen/output.csv", "r")
 genLines = genFile.readlines()
 genFile.close()
 
+expectedFile = open("gen/output.csv", "r")
+expectedLines = expectedFile.readlines()
+expectedFile.close()
+
 i = 0
 counter = 0
-for genOutput in genLines:
-    if destLines[i] == genOutput:
-        print "Line " + str(i) + ": match."
-        counter += 1
-    else:
-        print "Line " + str(i) + ": no match!"
-        print "Expected : " + genOutput.rstrip('\n') + " | Found :" + destLines[i].rstrip('\n')
+for expectedLine in expectedLines:
+    expectedLine = expectedLine.rstrip('\n')
+    expectedVal = expectedLine.split()
+    expectedValue = {"value": str(expectedVal[1]), "time": str(expectedVal[0])}
     i += 1
+    print "------------"
+    for realValue in realVals:
+        if realValue['time'] == expectedValue['time']:
+            if realValue['value'] == expectedValue['value']:
+                ''' found match '''
+                counter += 1
+                print "[Test] Sucess: Time[" + realValue['time'] + "] Value[" + realValue['value'] + "]"
+                realVals.remove(realValue)
+                ''' So no double success error '''
+            else:
+                '''time is right but not value error'''
+                print "[Test] Error: Time[" + realValue['time'] + "] | realValue[" + realValue[
+                    'value'] + "] - expectedValue[" + \
+                      expectedValue['value'] + "]"
 
+print "------------"
 print "Sucess rate for " + str(i) + " elements : " + str(float(100 * counter / i)) + "%"
